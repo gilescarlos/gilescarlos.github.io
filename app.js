de3ad058bb8885fbae6f0dd2b5b882954a58bd27
Giles Carlos <gilescarlos32@gmail.com>
2020-08-03 23:36:44 -0700

    Convert reviews to projects

--- app.js
+++ app.js
@@ -102,49 +102,31 @@ about.addEventListener('click', function (e) {
     }
 });
 // ********** projects ************
-const reviews = [
+const projects = [
     {
       id: 1,
-      name: "susan smith",
-      job: "web developer",
+      name: "Gymshark Bot",
+      language: "Python",
       img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+        "./images/Gymshark.png",
       text:
-        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+        "Created a script that automates the checkout process on the Gymshark website using the Selenium package for Python which is an open-source web-based automation tool.",
     },
     {
       id: 2,
-      name: "anna johnson",
-      job: "web designer",
+      name: "Solar Powered Portable Charger",
+      language: "Python",
       img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+        "./images/RaspberryPi.png",
       text:
-        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    },
-    {
-      id: 3,
-      name: "peter jones",
-      job: "intern",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-      text:
-        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    },
-    {
-      id: 4,
-      name: "bill anderson",
-      job: "the boss",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-      text:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+        "A fellow UCI student and I utilized a Servo Motor, Raspberry Pi 4, solar panels, and Python to create a portable charger.",
     },
   ];
   
   // select items by the id in the html file
-  const img = document.getElementById('person-img');
-  const author = document.getElementById('author');
-  const job = document.getElementById('job');
+  const img = document.getElementById('project-img');
+  const project = document.getElementById('project');
+  const language = document.getElementById('job');
   const info = document.getElementById('info');
   
   // store the button elements with the button variables
@@ -160,36 +142,36 @@ const reviews = [
     showPerson(currentItem);
   });
   
-  // show person based on item
-  function showPerson(personIndex) {
-    const item = reviews[personIndex];
+  // show project based on item
+  function showPerson(projectIndex) {
+    const item = projects[projectIndex];
     img.src = item.img;
-    author.textContent = item.name;
-    job.textContent = item.job;
+    project.textContent = item.name;
+    language.textContent = item.language;
     info.textContent = item.text;
   }
   
-  // show next person
+  // show next project
   nextBtn.addEventListener("click", function() {
     currentItem++;
-    if(currentItem >= reviews.length) {
+    if(currentItem >= projects.length) {
       currentItem = 0;
     }
     showPerson(currentItem);
   })
   
-  // show previous person
+  // show previous project
   prevBtn.addEventListener("click", function() {
     currentItem--;
     if(currentItem < 0) {
-      currentItem = reviews.length - 1;
+      currentItem = projects.length - 1;
     }
     showPerson(currentItem);
   })
   
-  // show random person
+  // show random project
   randomBtn.addEventListener("click", function() {
-    currentItem = Math.floor(Math.random()*reviews.length);
+    currentItem = Math.floor(Math.random()*projects.length);
     console.log(currentItem);
     showPerson(currentItem);
   })
